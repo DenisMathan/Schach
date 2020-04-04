38ed576c4d0535d4ded67ff35b08c696f38ec703
Denis Mathan <denis.mathan@googlemail.com>
2020-04-04 16:29:32 +0200

    der Turm ist drin

--- src/schach/Figuren/Turm.java
+++ src/schach/Figuren/Turm.java
@@ -8,20 +8,21 @@ public class Turm implements Figur {
 
     private int position[];
     private boolean moved = false;
-    private int [][] possibilities;
+    private int[][] possibilities = new int[28][2];
     private ImageIcon icon = new ImageIcon("icon.png");
     private Feld felder[][];
 
 
-    public Turm(int xpos, int ypos, Feld[][] felder){
-        this.position = new int [] {xpos, ypos};
+    public Turm(int xpos, int ypos, Feld[][] felder) {
+        this.position = new int[]{xpos, ypos};
         this.felder = felder;
     }
 
 
     @Override
     public void move(int xpos, int ypos) {
-
+        this.position=new int[]{xpos,ypos};
+        felder[xpos][ypos].setFigur(this);
     }
 
     @Override
@@ -31,16 +32,83 @@ public class Turm implements Figur {
 
     @Override
     public void possibilities() {
-        for (int z = 0; z<=3; z++)
-        for (int i = 0; i<=7; i++){
-            possibilities = new int[][] {
-                    {this.position[0], this.position[1]+i}
-            };
+
+        for (int i = 0; i <= 27; i++) {
+            if(i<=6){
+                //wandert die y-Achse hoch
+                possibilities[i][0] = this.position[0];
+                possibilities[i][1] = this.position[1] - (i+1);
+            }
+            else if(i>6 && i<=13){
+                //wandert die y-Achse herunter
+                possibilities[i][0] = this.position[0];
+                possibilities[i][1] = this.position[1] + (i-6);
+            }
+            else if(i>13 && i<=20){
+                //wandert die x-achse nach links
+                possibilities[i][0] = this.position[0]- (i-13);
+                possibilities[i][1] = this.position[1];}
+            else{
+                //wandert die x-Achse nach rechts
+                possibilities[i][0] = this.position[0] + (i-20);
+                possibilities[i][1] = this.position[1];
+            }
+            }
+
+
+
+        for (int i = 0; i <= 27; i++) {
+            //Abfage ob sich possibility auf dem Spielfeld befindet
+            if (possibilities[i][0] >= 0 && possibilities[i][0] <= 7 && possibilities[i][1] >= 0 && possibilities[i][1] <= 7) {
+                    if(i<=6){
+                        // abfrage ob sich Figur auf possibility befindet
+                        //wenn ja soll er gleich alle weiteren possibilities auf dieser Linie überspringen
+                        // -> bricht die rote Linie bei treffen auf figur ab
+                        if(this.felder[possibilities[i][0]][possibilities[i][1]].getFigur() == null){
+                            this.felder[possibilities[i][0]][possibilities[i][1]].setActive();
+                        }
+                        else{
+                            i = 6;
+                        }
+                    }
+                    else if(i>6 && i<=13){
+                        if(this.felder[possibilities[i][0]][possibilities[i][1]].getFigur() == null){
+                            this.felder[possibilities[i][0]][possibilities[i][1]].setActive();
+                        }
+                        else{
+                            i = 13;
+                        }
+                    }
+                    else if(i>13 && i<=20) {
+                        if(this.felder[possibilities[i][0]][possibilities[i][1]].getFigur() == null){
+                            this.felder[possibilities[i][0]][possibilities[i][1]].setActive();
+                        }
+                        else{
+                            i = 20;
+                        }
+                    }else{
+                        if(this.felder[possibilities[i][0]][possibilities[i][1]].getFigur() == null){
+                            this.felder[possibilities[i][0]][possibilities[i][1]].setActive();
+                        }
+                        else{
+                            i = 30;
+                        }
+                    }
+
+                }
+            }
         }
-    }
+
+
 
     @Override
     public void hidePossibilities() {
+        for (int i = 0; i<=27; i++){
+            if(possibilities[i][0] >= 0 && possibilities[i][0] <= 7 && possibilities[i][1] >= 0 && possibilities[i][1] <= 7 ){
+                if(this.felder[possibilities[i][0]] [possibilities[i][1]].getFigur() == null){
+                    this.felder[possibilities[i][0]] [possibilities[i][1]].setInActive();}
+            }
 
+        }
     }
 }
